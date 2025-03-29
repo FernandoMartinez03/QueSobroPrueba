2a00cd4f8d8330debf20ac7fdf55e8c6269a74f0
FernandoMartinez03 <a01568818@tec.mx>
2025-03-28 22:11:53 -0600

    Initial commit

---
+++ QueSobro/QueSobroApp.swift
@@ -0,0 +1,32 @@
+//
+//  QueSobroApp.swift
+//  QueSobro
+//
+//  Created by Fernando Martínez on 28/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QueSobroApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ QueSobroTests/QueSobroTests.swift
@@ -0,0 +1,17 @@
+//
+//  QueSobroTests.swift
+//  QueSobroTests
+//
+//  Created by Fernando Martínez on 28/03/25.
+//
+
+import Testing
+@testable import QueSobro
+
+struct QueSobroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
