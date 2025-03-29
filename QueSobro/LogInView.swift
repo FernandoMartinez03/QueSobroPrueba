8adeb790febf337034399816ebc35380cb6d065f
Adrian <a01568679@tec.mx>
2025-03-28 23:17:01 -0600

    Added Log In View

---
+++ QueSobro/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  QueSobro
+//
+//  Created by Alumno on 28/03/25.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
